a85acb9f6fe32903a9c553efd7348b1ea627301a
Sean Campbell <sean.campbell13@gmail.com>
2020-12-23 13:35:02 -0500

    gh-pages setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   trailingSlash: true,
   poweredByHeader: false,
   target: 'serverless',
+  assetPrefix: !debug ? 'seanpaulcampbell.com/' : '',
 }
